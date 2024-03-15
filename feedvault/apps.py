19241803f2a7aea28d405786a47c54d8f75951ea
Joakim Hellsén <tlovinator@gmail.com>
2024-03-15 14:16:06 +0100

    Use WAL

--- feedvault/apps.py
+++ feedvault/apps.py
@@ -1,4 +1,12 @@
+from typing import TYPE_CHECKING
+
 from django.apps import AppConfig
+from django.db.backends.signals import connection_created
+from django.db.backends.sqlite3.base import DatabaseWrapper
+from django.dispatch import receiver
+
+if TYPE_CHECKING:
+    from django.db.backends.utils import CursorWrapper
 
 
 class FeedVaultConfig(AppConfig):
@@ -6,3 +14,23 @@ class FeedVaultConfig(AppConfig):
 
     default_auto_field: str = "django.db.models.BigAutoField"
     name: str = "feedvault"
+
+
+@receiver(signal=connection_created)
+def activate_wal(sender: DatabaseWrapper, connection: DatabaseWrapper, **kwargs: dict) -> None:  # noqa: ARG001
+    """Activate Write-Ahead Logging (WAL) for SQLite databases.
+
+    WAL mode allows for concurrent reads and writes to the database.
+
+    We do this in apps.py because it is the earliest point in the
+    application startup process where we can be sure that the database
+    connection has been created.
+
+    Args:
+        sender: The sender of the signal.
+        connection: The connection to the database.
+        kwargs: Additional keyword arguments.
+    """
+    if connection.vendor == "sqlite":
+        cursor: CursorWrapper = connection.cursor()
+        cursor.execute(sql="PRAGMA journal_mode=WAL;")
